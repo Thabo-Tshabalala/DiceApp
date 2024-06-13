ebaebddf1b2913bf0c91e6b6acd103b9edcbbf6a
Thabo <justicethabo21@gmail.com>
2024-06-13 23:23:22 +0200

    First Commit

---
+++ meals_app/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp( MaterialApp(
+    home: Scaffold(body: Center(child: Image.asset("assets/images/dice1.png"),)),
+  ));
+}
+
